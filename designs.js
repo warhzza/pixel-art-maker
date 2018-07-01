fc8fdaf9f5c96382156f54cddc8483bd98227bbe
Abdullahi Usman <u.abdullahi21@yahoo.com>
2018-07-01 13:04:38 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,6 +1,6 @@
 /*
-This function builds the grid based on the inputs and it allows the user to
-click the cells of the grid to make pixels.
+    This function builds the grid based on the inputs and it allows the user to
+    click the cells of the grid to make pixels.
 */
 function makeGrid() {
     let gridRowStr, gridColStr, gridColInt, gridRowInt; // Variable declarations of the grid columns and rows
@@ -16,13 +16,16 @@ function makeGrid() {
 
     let tableElement = $('#pixelCanvas'); // Obtain the jQuery object of the table element
     tableElement.empty(); // Clean the children elements of table if there is any
+    
     let count = 0; // This variable will increase in a nested loop
+    
     /*
         Loop through the column value and append tr element
     */
     for (let col = 0; col < gridColInt; col++) {
         let t_col = "<tr></tr>";
         tableElement.append(t_col); // <table><tr></tr></table>
+        
         /*
             Loop through the row value and append td element to the last
             tr element
@@ -32,19 +35,18 @@ function makeGrid() {
             $('tr').last().append(t_row);  // <table><tr><td></td></tr></table>
 
             let t_data = "#td" + count; // This create an id used to identify each cells
+            
             /*
                 Register a click event to the current cell (td element) and change
                 the background to the currently selected picker.
             */
             $(t_data).on('click', function () {
-              console.log("td" + row);
               $(this).css('background-color', $('#colorPicker').val());
-            })
+            });
+            
             count = count + 1; // Increment the count
         }
-
     }
-
 }
 
 /* This function register a change event to the color picker input which
@@ -52,7 +54,6 @@ function makeGrid() {
 */
 function selectColor() {
     $('#colorPicker').on('change', function (event) {
-        // console.log(event.target.value);
         let colorValue = event.target.value; // Obtain the value of the selected color via the event object
         $('#colorPicker').val(colorValue); // Update the color input based on the selected color
     })
@@ -63,6 +64,7 @@ function selectColor() {
 */
 $(document).ready(function() {
     selectColor(); // Call the function that register a change event to the color picker input
+    
     /*
       Register a click event to the submit button of the form.
     */
@@ -71,8 +73,3 @@ $(document).ready(function() {
         makeGrid(); // Call the function that builds the grid
     });
 });
-
-
-
-
-
